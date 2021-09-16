1d6452619de6d2e5f41ff471b7fc7030d6201a2a
Rokas <rookas.rudzenskas@gmail.com>
2021-09-16 15:15:55 +0300

    Rokas

--- index.js
+++ index.js
@@ -40,3 +40,13 @@ const resolvers = {
     },
 };
 
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
+
