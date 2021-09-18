376152d2034f9e00eac0d966b3cbd00be12131aa
Rokas <rookas.rudzenskas@gmail.com>
2021-09-18 16:32:04 +0300

    Rokas

--- index.js
+++ index.js
@@ -256,7 +256,17 @@ const resolvers = {
     },
     TaskList: {
         id: ({ _id, id }) => _id || id,
-        progress: () => 0,
+        progress: async ({ _id }, _, { db }) => {
+            const todos = await db.collection('ToDo').find({taskListId: ObjectID(_id)}).toArray()
+
+            const completed = todos.filter(todo => todo.isCompleted);
+
+            if (todos.length === 0) {
+                return 0;
+            }
+
+            return 100 * completed.length / todos.length
+        },
         users: async ({userIds}, _, { db }) => Promise.all(userIds.map((userId) => (db.collection('Users').findOne({ _id: userId })))),
         todos: async ({ _id }, _, { db }) => (
             await db.collection('ToDo').find({ taskListId: ObjectID(_id)}).toArray()
